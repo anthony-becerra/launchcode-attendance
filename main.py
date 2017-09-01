dcef87305e660b9af9876df431ec9f7e0c272a62
Danny Neira <danny.neira@gmail.com>
2017-09-01 15:29:00 -0400

    error messages set to flash

--- main.py
+++ main.py
@@ -48,44 +48,43 @@ def teacher_signup():
 
         # check for empty fields
         if val.is_empty(first):
-            return render_template('teacher_login.html', title = 'Signup', 
-            signup='active', firstname_err = 'Please fill in the first name')
+            flash('Please fill in the first name')
+            return render_template('teacher_login.html', title = 'Signup', signup='active')
         elif val.is_empty(last):
-            return render_template('teacher_login.html', title = 'Signup',  
-                signup='active', lastname_err = 'Please fill in the last name')
+            flash('Please fill in the last name')
+            return render_template('teacher_login.html', title = 'Signup', signup='active')
         elif val.is_empty(email):
-            return render_template('teacher_login.html', title = 'Signup', 
-                signup='active',  email_err = 'Please fill in the email')
+            flash('Please fill in the email')
+            return render_template('teacher_login.html', title = 'Signup', signup='active')
         elif val.is_empty(password):
-            return render_template('teacher_login.html', title = 'Signup',  
-                signup='active', password_err = 'Please fill in the password')
+            flash('Please fill in the password')
+            return render_template('teacher_login.html', title = 'Signup', signup='active')
         
         # check for spaces
         if val.space(email):
-            return render_template('teacher_login.html', title = 'Signup',  
-                signup='active', email_err = 'Email can\'t have space')
+            flash('Email can\'t have space')
+            return render_template('teacher_login.html', title = 'Signup', signup='active')
 
         #check if email already exists
         if email_DB:
             if email_DB.email:
-                return render_template('teacher_login.html', title = 'Signup', 
-                signup='active', email_err = 'Email already in use')
+                flash('Email already in use')
+                return render_template('teacher_login.html', title = 'Signup', signup='active')
         
         # check for match
         if password != confirm_pass:
-            return render_template('teacher_login.html', title = 'Signup',  
-                signup='active', confirm_err = 'Passwords must match')
+            flash('Passwords must match')
+            return render_template('teacher_login.html', title = 'Signup')
 
         # checks length is bigger than 3 characters.
         if val.wrong_len(password) or val.wrong_len(confirm_pass):
-            return render_template('teacher_login.html', title = 'Signup', signup='active', 
-                password_err = 'Password must be longer than 3 characters')
+            flash('Password must be longer than 3 characters')
+            return render_template('teacher_login.html', title = 'Signup', signup='active')
         
         # Checks that email contains only one period after @ and only one @
         if val.wrong_email(email):
-            flash()
-            return render_template('teacher_login.html', title = 'Signup', signup='active', 
-                email_err = 'Email must contain only one @, one " . " after @')
+            flash('Email must contain only one @, one " . " after @')
+            return render_template('teacher_login.html', title = 'Signup', signup='active')
 
         new_teacher = Teacher(first, last, email, password)
         db.session.add(new_teacher)
@@ -103,8 +102,8 @@ def teacher_login():
             session['email'] = email
             return redirect('/')
         elif teacher and not check_hash(password, teacher.password):
-            return render_template('teacher_login.html', title = 'Login', login='active',
-                password_err = 'Wrong password')
+            flash("Wrong Password!")
+            return render_template('teacher_login.html', title = 'Login', login='active')
     else:
         return render_template('teacher_login.html', title = 'Login', login='active')
 
@@ -125,12 +124,12 @@ def start_day():
             return redirect('/student_login')
         else:
              # the day's list has not been created
-            return render_template('student_login.html', title = 'Student Login',
-                day_err = 'Today\'s attendance has\'s been created yet.')
+            flash('Today\'s attendance has\'s been created yet.')
+            return render_template('student_login.html', title = 'Student Login')
     else:
         # the day's list already created
-        return render_template('index.html', title = 'Attendance App',
-            day_err = 'Today\'s attendance already created')
+        flash('Today\'s attendance already created')
+        return render_template('index.html', title = 'Attendance App')
 
 
 @app.route('/student_login', methods=["POST", "GET"])
@@ -141,33 +140,33 @@ def student_login():
     if request.method == 'POST':
         student_id = request.form['student_id']
         pin = request.form['pin']
-        confirm_pin = request.form['confirm_pin']
         student = Student.query.get(student_id)
         student_att = Attendance.query.filter_by(owner_id = student_id,
                  date_now = date.today())
+
         if not pin:
-            return render_template('student_login.html', title ='Student Login', 
-                pin_err = "Please enter your Pin!", students = students)
+            flash("Please enter your Pin!")
+            return render_template('student_login.html', title ='Student Login', students = students)
         elif not pin.isdigit():
-            return render_template('student_login.html', title ='Student Login', 
-                pin_err = "Your Pin cannot have Letters!", students = students)
+            flash("Your Pin cannot have Letters!")
+            return render_template('student_login.html', title ='Student Login', students = students)
         elif student and student.pin != int(pin):
-            return render_template('student_login.html', title ='Student Login', 
-                pin_err = "Wrong Pin!", students = students)
+            flash("Wrong Pin!")
+            return render_template('student_login.html', title ='Student Login', students = students)
         else:
             # make student present in attendance table
             student_att.present = True
             db.session.commit()
-            return render_template('student_login.html', title ='Student Login', 
-                login_success="Signed in!", students = students)
+            flash("{0} Signed in!".format(student.first_name))
+            return render_template('student_login.html', title ='Student Login', students = students)
 
     else:
         attendance_exists = Attendance.query.filter_by(date_now = date.today()).first()
 
         # Validate if today's date exists in database
         if attendance_exists is None:
-            return render_template('index.html', title = 'Attendance App',
-                student_err = 'Please create today\'s attendance list first (Press \'START DAY\' button)')
+            flash('Please create today\'s attendance list first (Press \'START DAY\' button)')
+            return render_template('index.html', title = 'Attendance App')
         
         return render_template('student_login.html', title = 'Student Login', students = students)
 
